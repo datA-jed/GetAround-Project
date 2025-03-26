bdeeeb0dfc434c7df5e4a84a2a53137e658cae94
datA-jed <c.maulard@gmail.com>
2025-03-26 21:48:21 +0100

    ajout du script python app.py pour dashboard streamlit

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go
+import numpy as np
+
+DATA_URL = "https://raw.githubusercontent.com/datA-jed/GetAround-Project/refs/heads/main/processed_data.json"
+
+def load_data(nrows):
+    data = pd.read_json(DATA_URL)
+    return data.head(nrows)
+
+data = load_data(50)
+
+st.write(data)
